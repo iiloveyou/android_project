51bfd0796f44ec91d0343b642d4c0dbf843d9ce5
zhu li <645486759@qq.com>
2017-01-13 18:50:55 +0800

    Initial commit

---
+++ app/src/main/java/com/nuc/inpiration/activity/Login.java
@@ -0,0 +1,18 @@
+package com.nuc.inpiration.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import inspiration.nuc.com.inspiration.R;
+
+/**
+ * Created by 李柱 on 2017/1/10.
+ */
+
+public class Login extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
